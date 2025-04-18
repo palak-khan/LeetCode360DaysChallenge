37e27fc0e2b8caf6340f7582954d873641fb8409
Palak Khan <palakkhan2002@gmail.com>
2025-04-18 23:10:34 +0530

    add comment

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 //1: Running Sum of 1D Array
-let nums = [1,2,3,4,5]
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+let nums = [1, 2, 3, 4, 5];
 let sum = 0;
 for (let i = 0; i < nums.length; i++) {
   nums[i] = sum + nums[i];
