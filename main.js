0bdc7e9e5de08553467b660da628ec6f52b1a691
Palak Khan <palakkhan2002@gmail.com>
2025-04-26 10:51:00 +0530

    add python file

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //1: Running Sum of 1D Array
 // Input: nums = [1,2,3,4]
 // Output: [1,3,6,10]
-let nums = [1, 2, 3, 4];
-let sum = 0;
-for (let i = 0; i < nums.length; i++) {
-  nums[i] = sum + nums[i];
-  sum = nums[i];
-}
-console.log(nums);
+// let nums = [1, 2, 3, 4];
+// let sum = 0;
+// for (let i = 0; i < nums.length; i++) {
+//   nums[i] = sum + nums[i];
+//   sum = nums[i];
+// }
+// console.log(nums);
