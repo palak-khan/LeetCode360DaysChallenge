e38e01209200fded5599fe4cc92848892322edc2
Palak Khan <palakkhan2002@gmail.com>
2025-04-27 14:39:02 +0530

    valid palindrome

--- main.js
+++ main.js
@@ -8,3 +8,23 @@
 //   sum = nums[i];
 // }
 // console.log(nums);
+
+//2: Valid Palindrome
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+let phrase = "A man, a plan, a canal: Panama";
+phrase = phrase.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+console.log(phrase)
+let phraseReverse = phrase.split('').reverse().join('')
+console.log(phraseReverse)
+if(phrase === phraseReverse){
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
+
