e1b0a6bb451d94d4d3de0f3f723839826b69adc7
Palak Khan <palakkhan2002@gmail.com>
2025-07-31 21:34:06 +0530

    day8

--- 2951-Find-the-Peaks.js
+++ 2951-Find-the-Peaks.js
@@ -1,2 +1,13 @@
 // Date: 33-07-2025
 // Problem: 162. Find Peak Element
+// bruteforce approach 100% beat
+var findPeaks = function(mountain) {
+    let count = 0;
+    let arr = []
+    for(let i = 1; i < mountain.length - 1; i++){
+       if(mountain[i] > mountain [ i - 1] && mountain[i] > mountain [ i + 1]){
+         arr.push(i)
+        }
+    }
+    return arr;
+};
